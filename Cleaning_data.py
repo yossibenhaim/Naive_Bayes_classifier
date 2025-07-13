3f865ff658cdcc03a84f1cf87e9c35edcf36709c
Yossi Ben Haim <v0585204770@gmail.com>
2025-07-13 09:31:17 +0300

    added the cleaning_data'

---
+++ Cleaning_data.py
@@ -0,0 +1,20 @@
+class Cleaning_data:
+
+    def __int__(self):
+        self.data_frame = None
+
+    def cleaning_data(self, data):
+        self.data_frame = data
+        self.data_frame.set_index(self.choice_column())
+
+
+
+
+    def choice_column(self):
+        columns = self.data_frame.columns
+        stop_loop = True
+        while stop_loop:
+            column = input(f"send your choice in {columns}")
+            if column in columns:
+                stop_loop = False
+        return column
