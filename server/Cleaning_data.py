3e5de656a82916b198834ad8f03277feb64230b4
Yossi Ben Haim <v0585204770@gmail.com>
2025-07-16 13:16:37 +0300

    added the documentation in cleaning_data

--- server/Cleaning_data.py
+++ server/Cleaning_data.py
@@ -2,13 +2,22 @@ class Cleaning_data:
 
     def __init__(self, data_frame):
         """
-        Initializes the cleaning class with a DataFrame.
+        Initializes with a pandas DataFrame.
+
+        Args:
+            data_frame (pandas.DataFrame): The DataFrame to clean.
         """
         self.data_frame = data_frame
 
     def cleaning_data(self, name_column):
         """
-        Sets the given column as the index of the DataFrame.
+        Sets the specified column as the DataFrame's index.
+
+        Args:
+            name_column (str): The column name to set as index.
+
+        Returns:
+            pandas.DataFrame: DataFrame with new index set.
         """
         data_frame = self.data_frame.set_index(name_column)
         return data_frame
